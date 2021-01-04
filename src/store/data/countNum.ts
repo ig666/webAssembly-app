22496cef8783df011c3c2f8205f57d55bfed6dd3
sunshixiong <1285913468@qq.com>
2021-01-04 15:05:22 +0800

    测试

--- src/store/data/countNum.ts
+++ src/store/data/countNum.ts
@@ -3,12 +3,12 @@ class MyState {
     @observable public num1 = 0;
     @observable public num2 = 100;
     @action public addNum1 = () => {
-        this.num1 = 5   ;
+        this.num1 += 1;
     };
     @action public addNum2 = () => {
         this.num2 += 1;
     };
-    @computed public get total() {
+    @computed public get total(){
         return this.num1 + this.num2
     }
 }
