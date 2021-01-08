4096f549d0cc467bb169441736d623bc6821416c
qi.chen <nx-7416b>
2021-01-08 10:37:17 +0800

    modify 加入洛蒂动画库

--- src/pages/user/user.tsx
+++ src/pages/user/user.tsx
@@ -1,9 +1,23 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect, useRef } from 'react'
 import { RouteChildrenProps } from 'react-router-dom'
+import lottie from 'lottie-web'
+import animationJsonData from '../../animationJson/43792-yoga-se-hi-hoga.json'
 
 const User: FC<RouteChildrenProps> = (props) => {
+    const node = useRef<HTMLDivElement>(null)
+    useEffect(() => {
+        const lot = lottie.loadAnimation({
+            container: node.current!,
+            renderer: 'svg',
+            loop: true,
+            autoplay: true,
+            animationData: animationJsonData
+        })
+        lot.play()
+        return () => { lot.stop() }
+    }, [])
     return (
-        <div>user</div>
+        <div ref={node} style={{ height: '500px' }}></div>
     )
 }
 
