f87365d5e3eb12b33a59195d119dbdac726eb9b9
qi.chen <nx-7416b>
2021-02-01 17:42:24 +0800

    modify 用户模块完成

--- src/pages/user/user.tsx
+++ src/pages/user/user.tsx
@@ -1,9 +1,20 @@
-import { Card, message } from "antd";
 import { FC, useState } from "react";
-import { Form, Row, Col, Input, Button, Table, Space, Popconfirm } from "antd";
+import {
+  Card,
+  message,
+  Form,
+  Row,
+  Col,
+  Input,
+  Button,
+  Table,
+  Space,
+  Popconfirm,
+  Modal,
+} from "antd";
 import { useRequest } from "ahooks";
 import { handleService } from "../../utils/request";
-import dayjs from 'dayjs'
+import dayjs from "dayjs";
 
 interface UserProps {
   id: string;
@@ -11,8 +22,8 @@ interface UserProps {
   gender: number;
   username: string;
   nickname: string;
-  createTime:string,
-  updateTime:string
+  createTime: string;
+  updateTime: string;
 }
 enum gender {
   男 = 1,
@@ -22,8 +33,12 @@ enum gender {
 const { Column } = Table;
 
 const User: FC = () => {
+  //状态类
   const [searchData, setSearchData] = useState({});
+  const [modalVisible, setModalVisible] = useState(false);
   const [form] = Form.useForm();
+  const [modalForm] = Form.useForm();
+  //请求类
   const { tableProps, refresh } = useRequest(
     ({ current, pageSize }) => {
       return handleService({
@@ -37,11 +52,16 @@ const User: FC = () => {
       paginated: true,
       refreshDeps: [searchData],
       formatResult: (result: any) => {
+        if (!result) return;
         const { data } = result;
         data.list = data.list.map((item: UserProps, index: number) => {
           item.key = index;
-          item.createTime=dayjs(item.createTime).format('YYYY-MM-DD HH:mm:ss')
-          item.updateTime=dayjs(item.updateTime).format('YYYY-MM-DD HH:mm:ss')
+          item.createTime = dayjs(item.createTime).format(
+            "YYYY-MM-DD HH:mm:ss"
+          );
+          item.updateTime = dayjs(item.updateTime).format(
+            "YYYY-MM-DD HH:mm:ss"
+          );
           return item;
         });
         return data;
@@ -57,7 +77,18 @@ const User: FC = () => {
       }
     },
   });
-
+  const modalRequest = useRequest(handleService, {
+    manual: true,
+    onSuccess: (result) => {
+      if (result) {
+        setModalVisible(false);
+        modalForm.resetFields();
+        message.success("编辑成功");
+        refresh();
+      }
+    },
+  });
+  //方法类
   const getFields = () => {
     let children = (
       <Col span={8}>
@@ -68,16 +99,15 @@ const User: FC = () => {
     );
     return children;
   };
-  const onFinish = (values: any) => {
-    setSearchData(values);
-  };
   const formSearch = () => {
     return (
       <Form
         form={form}
         name="advanced_search"
         className="ant-advanced-search-form"
-        onFinish={onFinish}
+        onFinish={(values) => {
+          setSearchData(values);
+        }}
       >
         <Row gutter={24}>{getFields()}</Row>
         <Row>
@@ -106,61 +136,117 @@ const User: FC = () => {
       `${range[0]}-${range[1]} of ${total} items`;
     return tableProps;
   };
-  const deleteUser = (id: string) => {
-    run({ data: { id }, method: "DELETE", url: "deleteUser" });
+  const updateUser = (row: any) => {
+    setModalVisible(() => {
+      modalForm.setFieldsValue(row);
+      return true;
+    });
   };
-  const updateUser = (id: string) => {
-    console.log(id);
+  const renderAction = (text: string, record: UserProps) => {
+    return (
+      <Space size="middle">
+        <Button
+          type="primary"
+          onClick={() => {
+            updateUser(record);
+          }}
+        >
+          编辑
+        </Button>
+        <Popconfirm
+          title="确认删除?"
+          onConfirm={() => {
+            run({
+              data: { id: record.id },
+              method: "DELETE",
+              url: "deleteUser",
+            });
+          }}
+          onCancel={() => {
+            console.log("点击了取消");
+          }}
+          okText="确认"
+          cancelText="取消"
+        >
+          <Button>删除</Button>
+        </Popconfirm>
+      </Space>
+    );
   };
   return (
     <Card>
       {formSearch()}
+
+      {/* S table */}
       <Table<UserProps>
         {...formatterPagination()}
         scroll={{ scrollToFirstRowOnChange: true, x: 1500, y: 550 }}
       >
-        <Column<UserProps> title="姓名" dataIndex="username" key="age" />
-        <Column<UserProps>
+        <Column title="姓名" dataIndex="username" key="age" />
+        <Column
           title="性别"
           dataIndex="gender"
           key="gender"
           render={(text) => <>{gender[text]}</>}
         />
-        <Column<UserProps> title="昵称" dataIndex="nickname" key="nickname" />
-        <Column<UserProps> title="创建时间" dataIndex="createTime" key="createTime" />
-        <Column<UserProps> title="修改时间" dataIndex="updateTime" key="updateTime" />
+        <Column title="昵称" dataIndex="nickname" key="nickname" />
+        <Column title="创建时间" dataIndex="createTime" key="createTime" />
+        <Column title="修改时间" dataIndex="updateTime" key="updateTime" />
         <Column<UserProps>
           title="操作"
           width={200}
           key="action"
           fixed="right"
-          render={(text, record) => (
-            <Space size="middle">
-              <Button
-                type="primary"
-                onClick={() => {
-                  updateUser(record.id);
-                }}
-              >
-                编辑
-              </Button>
-              <Popconfirm
-                title="确认删除?"
-                onConfirm={() => {
-                  deleteUser(record.id);
-                }}
-                onCancel={() => {
-                  console.log("点击了取消");
-                }}
-                okText="确认"
-                cancelText="取消"
-              >
-                <Button>删除</Button>
-              </Popconfirm>
-            </Space>
-          )}
+          render={renderAction}
         />
       </Table>
+      {/* E table */}
+
+      {/* S 弹窗 */}
+      <Modal
+        title="编辑用户"
+        visible={modalVisible}
+        onOk={() => {
+          modalForm.submit();
+        }}
+        okButtonProps={{ loading: modalRequest.loading }}
+        onCancel={() => {
+          setModalVisible(false);
+          modalForm.resetFields();
+        }}
+      >
+        <Form
+          form={modalForm}
+          name="control-hooks"
+          labelCol={{ span: 3 }}
+          wrapperCol={{ span: 21 }}
+          onFinish={(values) => {
+            modalRequest.run({
+              data: values,
+              method: "POST",
+              url: "updateUser",
+            });
+          }}
+        >
+          <Form.Item name={"id"} hidden>
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name="username"
+            label="用户名"
+            rules={[{ required: true, message: "请输入用户名称!" }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item name="nickname" label="昵称">
+            <Input />
+          </Form.Item>
+          <Form.Item name="password" label="密码">
+            <Input />
+          </Form.Item>
+        </Form>
+      </Modal>
+      {/* E 弹窗 */}
     </Card>
   );
 };
